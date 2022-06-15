08e2288c76c8ef485fde2952a7e1a3d0ff4eb84a
Berislav Crkvenac <cberislav@gmail.com>
2022-06-15 14:49:00 +0200

    Mijenjam pwd sumanuto

--- WebAPI/BasicUserValidationService.cs
+++ WebAPI/BasicUserValidationService.cs
@@ -20,7 +20,7 @@ public class BasicUserValidationService : IBasicUserValidationService
 			//var user = await _userRepository.GetUserByUsername(username);
 			//var isValid = user != null && user.Password == password;
 			//return isValid;
-			return username == "test" && password == "ber_branch2";
+			return username == "test" && password == "bbbbbbb";
 		}
 		catch (Exception e)
 		{
