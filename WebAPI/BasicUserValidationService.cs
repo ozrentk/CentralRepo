5be2e79836cb0da3241a19c91cf995b0e90a294e
Berislav Crkvenac <cberislav@gmail.com>
2022-06-15 14:32:23 +0200

    Berislav pwd promjena test

--- WebAPI/BasicUserValidationService.cs
+++ WebAPI/BasicUserValidationService.cs
@@ -20,7 +20,7 @@ public class BasicUserValidationService : IBasicUserValidationService
 			//var user = await _userRepository.GetUserByUsername(username);
 			//var isValid = user != null && user.Password == password;
 			//return isValid;
-			return username == "test" && password == "srfewsr";
+			return username == "test" && password == "ber";
 		}
 		catch (Exception e)
 		{
