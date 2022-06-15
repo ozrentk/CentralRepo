3d64c042ea9d005cf6df30b357ce54e5f37bacf4
Vlatko <vkristo1@gmail.com>
2022-06-15 14:22:03 +0200

    Novi 5

--- WebAPI/BasicUserValidationService.cs
+++ WebAPI/BasicUserValidationService.cs
@@ -20,7 +20,7 @@ public class BasicUserValidationService : IBasicUserValidationService
 			//var user = await _userRepository.GetUserByUsername(username);
 			//var isValid = user != null && user.Password == password;
 			//return isValid;
-			return username == "test" && password == "123!test";
+			return username == "test" && password == "123!test5";
 		}
 		catch (Exception e)
 		{
