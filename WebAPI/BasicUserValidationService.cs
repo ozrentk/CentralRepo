58bd3706b31d4461fcd5f35d469716f51a9a6730
Berislav Crkvenac <cberislav@gmail.com>
2022-06-15 15:19:28 +0200

    Izmjena

--- WebAPI/BasicUserValidationService.cs
+++ WebAPI/BasicUserValidationService.cs
@@ -20,8 +20,7 @@ public class BasicUserValidationService : IBasicUserValidationService
 			//var user = await _userRepository.GetUserByUsername(username);
 			//var isValid = user != null && user.Password == password;
 			//return isValid;
-
-			return username == "test" && password == "bbbbbbb22";
+			return username == "test" && password == "branch31";
 		}
 		catch (Exception e)
 		{
