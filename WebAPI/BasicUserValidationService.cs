81dc444e672b05deb20cecc51a98a12e4faa1648
Berislav Crkvenac <cberislav@gmail.com>
2022-06-15 14:45:40 +0200

    Moj commit prije push

--- WebAPI/BasicUserValidationService.cs
+++ WebAPI/BasicUserValidationService.cs
@@ -20,7 +20,7 @@ public class BasicUserValidationService : IBasicUserValidationService
 			//var user = await _userRepository.GetUserByUsername(username);
 			//var isValid = user != null && user.Password == password;
 			//return isValid;
-			return username == "test" && password == "ber";
+			return username == "test" && password == "ber_branch2";
 		}
 		catch (Exception e)
 		{
